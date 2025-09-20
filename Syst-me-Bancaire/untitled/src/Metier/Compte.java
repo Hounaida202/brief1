5bf4937a1eea1f2938c7e7300e115e4f2f6b4282
Hounaida202 <etrichih@gmail.com>
2025-09-20 20:15:00 +0100

    l ajout des attributs

--- Syst-me-Bancaire/untitled/src/Metier/Compte.java
+++ Syst-me-Bancaire/untitled/src/Metier/Compte.java
@@ -1,4 +1,11 @@
 package Metier;
 
+import java.util.ArrayList;
+
 public abstract class Compte {
+    protected String code;
+    protected double solde;
+    protected ArrayList<Operation> operations;
+    protected String type;
+    private static int compteur = 1;
 }
